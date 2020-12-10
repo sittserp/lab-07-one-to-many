b18c6f319539cb0cc2630ce146a860d79c49221b
Perry Sittser <sittserp@gmail.com>
2020-12-09 16:34:40 -0800

    Boat CRUD routes tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
+const Boat = require('../lib/models/Boat');
 const pool = require('../lib/utils/pool');
 
 describe('app endpoint', () => {
@@ -22,8 +23,85 @@ describe('app endpoint', () => {
       });
 
     expect(res.body).toEqual({ 
+      id: '1',
       style: 'canoe',
       color: 'red',
       powered: 'paddle' });
+  });
+
+  it ('finds a boat by id via GET', async() => {
+    const boat = await Boat.insert({
+      style: 'canoe',
+      color: 'red',
+      powered: 'paddle'
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/boats/${boat.id}`);
+
+    expect(res.body).toEqual(boat);
+  });
+
+  it('finds all boats via GET', async() => {
+    const boats = await Promise.all([
+      {
+        style: 'canoe',
+        color: 'red',
+        powered: 'paddle'
+      },
+      {
+        style: 'submarine',
+        color: 'black',
+        powered: 'nuclear'
+      },
+      {
+        style: 'oil tanker',
+        color: 'navy blue',
+        powered: 'oil'
+      }
+
+    ].map(boat => Boat.insert(boat)));
+
+    const res = await request(app)
+      .get('/api/v1/boats');
+
+    expect(res.body).toEqual(expect.arrayContaining(boats));
+    expect(res.body).toHaveLength(boats.length);
+  });
+
+  it('updates a boat via PUT', async() => {
+    const boat = await Boat.insert({
+      style: 'canoe',
+      color: 'red',
+      powered: 'paddle'
+    });
+
+    const res = await request(app)
+      .put(`/api/v1/boats/${boat.id}`)
+      .send({
+        style: 'ship',
+        color: 'blue',
+        powered: 'steam'
+      });
+
+    expect(res.body).toEqual({
+      id: boat.id,
+      style: 'ship',
+      color: 'blue',
+      powered: 'steam'
+    });
+  });
+
+  it('deletes a boat via DELETE', async() => {
+    const boat = await Boat.insert({
+      style: 'canoe',
+      color: 'red',
+      powered: 'paddle'
+    });
+
+    const res = await request(app)
+      .delete(`/api/v1/boats/${boat.id}`);
+
+    expect(res.body).toEqual(boat);
   });
 });

9910059fe63aaeeeb94326a8b5b2fe6858d99125
Perry Sittser <sittserp@gmail.com>
2020-12-10 14:22:27 -0800

    All tests pass!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -32,28 +32,6 @@ describe('app endpoint', () => {
       powered: 'paddle' });
   });
 
-  it ('finds a boat by id and associated voyages via GET', async() => {
-    const boat = await Boat.insert({
-      style: 'canoe',
-      color: 'red',
-      powered: 'paddle'
-    });
-
-    const voyages = await Promise.all([
-      { duration: 10, boatId: boat.id },
-      { duration: 55, boatId: boat.id },
-      { duration: 15, boatId: boat.id },
-    ].map(voyage => Voyage.insert(voyage)));
-
-    const res = await request(app)
-      .get(`/api/v1/boats/${boat.id}`);
-
-    expect(res.body).toEqual({
-      ...boat,
-      voyages: expect.arrayContaining(voyages)
-    });
-  });
-
   it('finds all boats via GET', async() => {
     const boats = await Promise.all([
       {
@@ -79,6 +57,28 @@ describe('app endpoint', () => {
 
     expect(res.body).toEqual(expect.arrayContaining(boats));
     expect(res.body).toHaveLength(boats.length);
+  });
+
+  it ('finds a boat by id and all associated voyages via GET', async() => {
+    const boat = await Boat.insert({
+      style: 'canoe',
+      color: 'red',
+      powered: 'paddle'
+    });
+
+    const voyages = await Promise.all([
+      { duration: 10, boatId: boat.id },
+      { duration: 55, boatId: boat.id },
+      { duration: 15, boatId: boat.id },
+    ].map(voyage => Voyage.insert(voyage)));
+
+    const res = await request(app)
+      .get(`/api/v1/boats/${boat.id}`);
+
+    expect(res.body).toEqual({
+      ...boat,
+      voyages: expect.arrayContaining(voyages)
+    });
   });
 
   it('updates a boat via PUT', async() => {
@@ -135,12 +135,55 @@ describe('app endpoint', () => {
 
     expect(res.body).toEqual({ 
       id: '1',
-      duration: 10,
+      duration: '10',
       boatId: '1' });
   });
 
-  it('deletes a voyage via DELETE', async() => {
+  it('finds all voyages via GET', async() => {
 
+    const boats = await Promise.all([
+      {
+        style: 'canoe',
+        color: 'red',
+        powered: 'paddle'
+      },
+      {
+        style: 'submarine',
+        color: 'black',
+        powered: 'nuclear'
+      },
+      {
+        style: 'oil tanker',
+        color: 'navy blue',
+        powered: 'oil'
+      }
+
+    ].map(boat => Boat.insert(boat)));
+
+    const voyages = await Promise.all([
+      {
+        duration: '10',
+        boatId: boats[0].id,
+      },
+      {
+        duration: '20',
+        boatId: boats[1].id,
+      },
+      {
+        duration: '30',
+        boatId: boats[2].id,
+      }
+
+    ].map(voyage => Voyage.insert(voyage)));
+
+    const res = await request(app)
+      .get('/api/v1/voyages');
+
+    expect(res.body).toEqual(expect.arrayContaining(voyages));
+    expect(res.body).toHaveLength(voyages.length);
+  });
+
+  it ('finds a voyage by id via GET', async() => {
     const boat = await Boat.insert({
       style: 'canoe',
       color: 'red',
@@ -153,9 +196,13 @@ describe('app endpoint', () => {
     });
 
     const res = await request(app)
-      .delete(`/api/v1/voyages/${voyage.id}`);
+      .get(`/api/v1/voyages/${voyage.id}`);
 
-    expect(res.body).toEqual(voyage);
+    expect(res.body).toEqual({
+      id: voyage.id,
+      duration: '10',
+      boatId: boat.id
+    });
   });
 
   it('updates a voyage via PUT', async() => {
@@ -182,6 +229,25 @@ describe('app endpoint', () => {
       duration: '20',
       boatId: boat.id
     });
+  });
+
+  it('deletes a voyage via DELETE', async() => {
+
+    const boat = await Boat.insert({
+      style: 'canoe',
+      color: 'red',
+      powered: 'paddle'
+    });
+
+    const voyage = await Voyage.insert({
+      duration: '10',
+      boatId: boat.id
+    });
+
+    const res = await request(app)
+      .delete(`/api/v1/voyages/${voyage.id}`);
+
+    expect(res.body).toEqual(voyage);
   });
 
 });
